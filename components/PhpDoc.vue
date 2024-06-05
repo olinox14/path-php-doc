a836a30e587c756568966ac7b9a0068d35232f0b
olinox14 <olinox14@tuta.io>
2024-06-05 22:52:47 +0200

    cleanup fetched html from phpdoc

--- components/PhpDoc.vue
+++ components/PhpDoc.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-html="docCode" />
+    <div v-html="docCode" class="doc" />
   </div>
 </template>
 
@@ -14,9 +14,36 @@ const response = await fetch('https://raw.githubusercontent.com/olinox14/path-ph
 const html = await response.text()
 const $ = cheerio.load(html)
 
-docCode.value = $('body').html()
+$('.phpdocumentor-breadcrumbs').remove()
+$('.phpdocumentor-element__package').remove()
+$('.phpdocumentor-element-found-in').remove()
+$('#toc').remove()
+$('.phpdocumentor-tag-list__heading').remove()
+$('.phpdocumentor-tag-list').remove()
+$('#toc-properties').remove()
+$('.phpdocumentor-table-of-contents').remove()
+$('#toc-methods').remove()
+$('.phpdocumentor-properties').remove()
+$('.phpdocumentor-modal').remove()
+
+docCode.value = $('.phpdocumentor-content').html()
 </script>
 
 <style scoped lang="scss">
+.doc {
+  display: flex;
+  flex-direction: row;
 
+  :deep(.phpdocumentor-element) {
+    min-width: 80%;
+    width: 80%;
+    max-width: 80%;
+  }
+
+  :deep(.phpdocumentor-on-this-page__sidebar) {
+    min-width: 20%;
+    width: 20%;
+    max-width: 20%;
+  }
+}
 </style>
