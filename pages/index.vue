59847dd0b328e3e21912ad349f4a4b0294beab93
olinox14 <olinox14@tuta.io>
2024-06-13 23:15:27 +0200

    add examples to home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,23 @@
 
   <p>An intuitive, standalone, and object-oriented library for file and path operations.</p>
 
-  <CodeBlock :code="code" language="php" />
+  <CodeBlock :code="codeStart" language="php" />
+
+  <p>Manipulate your file paths with ease :</p>
+
+  <CodeBlock :code="codeManipulate" language="php" />
+
+  <p>Browse directories and files :</p>
+
+  <CodeBlock :code="codeBrowse" language="php" />
+
+  <p>Perform all file operations allowed by PHP's builtin functions :</p>
+
+  <CodeBlock :code="codeBuiltinOperations" language="php" />
+
+  <p>And many more :</p>
+
+  <CodeBlock :code="codeAdditionalOperations" language="php" />
 
   <div class="d-flex justify-center">
     <v-btn to="get-started" class="bg-primary">Get started</v-btn>
@@ -13,23 +29,37 @@
 
 <script setup lang="ts">
 
-const code = `
-<?php
+const codeStart = `<?php
 use Path\\Path;
 
-$dir = new Path('/home/user/workdir');
+$dir = new Path('/home/user/myFile.txt');
+`
+
+const codeManipulate = `$dir = new Path('/home/user/workdir');
+
+$newPath = $dir->append('foo.txt');
+
+$parentDir = $dir->parent();
+`
+
+const codeBrowse = `$dir = new Path('/home/user/workdir');
 
 foreach ($dir->files() as $file) {
-    $file->chmod(755);
+    // ...
 }
 
-$fooDir = $dir->append('foo');
+foreach ($dir->dirs() as $dir) {
+    // ...
+}
+`
 
-$fooDir->mkdir();
+const codeBuiltinOperations = `$file = new Path('/home/user/file.txt');
+$file->chmod(0777);
+`
 
-$newFile = $fooDir->append('my-file.txt');
-
-$newFile->putContent('my new content');
+const codeAdditionalOperations = `$dir = new Path('/home/user/foo');
+$target = new Path('/home/user/bar');
+$dir->copyTree($target);
 `
 </script>
 
