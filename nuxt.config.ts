7dd3344e98245bd7c9150771aab0533f5fbf38f3
olinox14 <olinox14@tuta.io>
2024-06-17 00:06:34 +0200

    restore head config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,23 @@ export default defineNuxtConfig({
     // @see https://github.com/nuxt/devtools
     enabled: true,
   },
+  app: {
+    head: {
+      title: 'Path-PHP Documentation',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;700&display=swap',
+        },
+      ],
+    },
+  },
   build: {
     transpile: ['vuetify'],
   },
